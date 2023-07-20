f0bf156e9810968c8b834b7f4b2b9be7ccc7ccd2
kevin.alvarez <kevin.alvarez@agrizar.com>
2023-07-20 08:02:58 -0600

    20/07/23

--- app/src/main/java/com/example/berriesconteo/subirDatosBD.kt
+++ app/src/main/java/com/example/berriesconteo/subirDatosBD.kt
@@ -1,25 +1,20 @@
 package com.example.berriesconteo
 
+import android.database.Cursor
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class subirDatosBD:AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-        setContentView(R.layout.activity_main)
 
-        val btnsubirDatos = findViewById<Button>(R.id.btnSubir)
-        btnsubirDatos.setOnClickListener{
-            exportarDatos()
-        }
-    }
-    private fun exportarDatos() {
-        val dbHandler = DBBerries((this)," DBBerries", null,9 )
-        val data = dbHandler.obtenerDatosComoCSV()
+    var arrModuloTitulos : MutableList<String>? = mutableListOf()
 
-        val csvFile = File(filesDir, "data.csv")
-        csvFile.writeText(data)
-    }
+    var dbBerries = DBBerries(applicationContext," DBBerries", null, 9);
+    val db = dbBerries.readableDatabase
+
+    val columns = arrayOf("idmodulo","nombremodulo")
+
+    val cursorModulo: Cursor = db.query("modulosberries", columns, null, null, null, null, "idmodulo ASC")
+
 }
